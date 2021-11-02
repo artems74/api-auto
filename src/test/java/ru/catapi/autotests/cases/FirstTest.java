136c03c24bf1aee9204dbd9b0e3fce423f3cbbc4
Artem Sirota <sirota@virtualhealth.com>
2021-11-03 02:28:36 +0500

    Removed redundant lines from tests classes

--- src/test/java/ru/catapi/autotests/cases/FirstTest.java
+++ src/test/java/ru/catapi/autotests/cases/FirstTest.java
@@ -28,54 +28,36 @@ public class FirstTest {
 
     @Step("Step 1: Get breed id")
     public void getBreedId1Step(String breedName) {
-        System.out.println("STEP 1: START");
         this.breedId = BreedApiService.getBreedIdByName(breedName);
-        System.out.println("STEP 1: " + this.breedId);
-        System.out.println("STEP 1: END");
     }
 
     @Step("Step 2: Get image id, url")
     public void getBreedId2Step() {
-        System.out.println("STEP 2: START");
         ImageResponse image = ImageApiService.getImageByBreedId(this.breedId);
         this.imageId = image.getId();
         this.imageUrl = image.getUrl();
-        System.out.println("STEP 2: " + this.imageId);
-        System.out.println("STEP 2: " + this.imageUrl);
-        System.out.println("STEP 2: END");
     }
 
     @Step("Step 3: Post image to favourites")
     public void postImgToFavourites3Step() {
-        System.out.println("STEP 3: START");
         this.favouriteId = FavouriteApiService.postFavouriteImageById(this.imageId);
-        System.out.println("STEP 3: " + this.favouriteId);
-        System.out.println("STEP 3: END");
     }
 
     @Step("Step 4: Get favourite images")
     public void getFavouriteImg4Step() {
-        System.out.println("STEP 4: START");
         FavouritesResponse favouritesResponse = FavouriteApiService.getFavouritesImages();
-        System.out.println(favouritesResponse.getFavouriteId());
         Assert.assertTrue(favouritesResponse.getFavouriteId().contains(this.favouriteId));
         Assert.assertTrue(favouritesResponse.getImageId().contains(this.imageId));
-        System.out.println("STEP 4: END");
     }
 
     @Step("Step 5: Delete favourite image")
     public void deleteFavouriteImage5Step() {
-        System.out.println("STEP 5: START");
         FavouriteApiService.deleteFavouriteImageById(this.favouriteId);
-        System.out.println("STEP 5: END");
     }
 
     @Step("Step 6:")
     public void getFavouriteImg6Step() {
-        System.out.println("STEP 6: START");
         FavouritesResponse favouritesResponse = FavouriteApiService.getFavouritesImages();
-        System.out.println(favouritesResponse.getFavouriteId());
         Assert.assertFalse(favouritesResponse.getFavouriteId().contains(this.favouriteId));
-        System.out.println("STEP 6: END");
     }
 }
